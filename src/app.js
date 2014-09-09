66053c3670e70bd5e132c065d288314de9bcf053
Lenny Khazan <lenny.khazan@gmail.com>
2014-09-08 20:19:26 -0400

    Did things, fixed stuffs

--- src/app.js
+++ src/app.js
@@ -1,70 +1,60 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
+var conf = {
+  LIVEBLOG_URL: 'http://live.theverge.com/apple-iphone-iwatch-live-blog/live.json',
+  LIVEBLOG_NAME: 'iPhone 6 Event',
+  REFRESH_INTERVAL: 30 /* seconds */
+};
 
-function main() {
+var UI = require('ui');
+var ajax = require('ajax');
 
-  var conf = {
-    LIVEBLOG_URL: 'http://live.theverge.com/apple-iphone-iwatch-live-blog/live.json',
-    LIVEBLOG_NAME: 'iPhone 6 Event',
-    REFRESH_INTERVAL: 30 /* seconds */
-  };
+var HTML5Entities = require('html5-entities.js');
+var Html5Entities = new HTML5Entities();
 
-  var UI = require('ui');
-  var ajax = require('ajax');
+var mainCard = new UI.Card({
+  title: conf.LIVEBLOG_NAME + ' Liveblog',
+  subtitle: 'theverge.com',
+  body: 'Press select to refresh.'
+});
 
-  var HTML5Entities = require('html5-entities.js');
-  var Html5Entities = new HTML5Entities();
-
-  var mainCard = new UI.Card({
-    title: conf.LIVEBLOG_NAME + ' Liveblog',
-    body: 'Press select to refresh.'
+var addCard = function(entry) {
+  var body = entry.body.replace(/(<([^>]+)>)/ig, '');
+  body.trim();
+  if (body.length === 0) return;
+  var card = new UI.Card({
+    body: Html5Entities.decode(body),
+    scrollable: true
   });
   
-  var addCard = function(entry) {
-    var body = entry.body.replace(/(<([^>]+)>)/ig, '');
-    body.trim();
-    if (body.length === 0) return;
-    var card = new UI.Card({
-      body: Html5Entities.decode(body),
-      scrollable: true
-    });
-    
-    card.on('click', 'select', function() {
-      card.hide();
-    });
-  
-    card.show();
-  };
+  card.on('click', 'select', function() {
+    card.hide();
+  });
 
-  var lastShownId = -1;
+  card.show();
+};
 
-  var refreshData = function() {
-    mainCard.body('Downloading new messages...');
-    ajax({url: conf.LIVEBLOG_URL, type: 'json'}, function(data) {
-      if (!data) {
-        mainCard.body('[!] Something went wrong.');
-      } else {
-        for (var i = data.entries.length-1; i >= 0; i--) {
-          if (data.entries.length - i > 15) break; //prevent too many messages
-          var entry = data.entries[i];
-          if (!entry.pinned && entry.id > lastShownId) {
-            addCard(entry);
-          }
+var lastShownId = -1;
+
+var refreshData = function() {
+  mainCard.body('Downloading new messages...');
+  ajax({url: conf.LIVEBLOG_URL, type: 'json'}, function(data) {
+    if (!data) {
+      mainCard.body('[!] Something went wrong.');
+    } else {
+      for (var i = data.entries.length-1; i >= 0; i--) {
+        if (data.entries.length - i > 15) break; //prevent too many messages
+        var entry = data.entries[i];
+        if (!entry.pinned && entry.id > lastShownId) {
+          addCard(entry);
         }
-        lastShownId = data.entries[data.entries.length-1].id;
-      
-        mainCard.body('Press select to refresh.');
       }
-    });
-  };
+      lastShownId = data.entries[data.entries.length-1].id;
+    
+      mainCard.body('Press select to refresh.');
+    }
+  });
+};
 
-  mainCard.on('click', 'select', refreshData);
-  mainCard.show();
+mainCard.on('click', 'select', refreshData);
+mainCard.show();
 
-  refreshData();
-}
-
-main();
+refreshData();
